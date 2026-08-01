5f1af105a6053e4f7b46105bcbef5a159157da9d
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-02 00:13:09 +0600

    Add Queue at the School [B] from Codeforces

---
+++ 266/B - Queue at the School/solution.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d%d",&n,&t);
+    char s[n];
+    scanf("%s",s);
+    while(t--)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]=='B'&&s[i+1]=='G')
+            {
+                s[i]='G';
+                s[i+1]='B';
+                i++;
+            }
+ 
+        }
+    }
+    printf("%s",s);
+ 
+    return 0;
+}
