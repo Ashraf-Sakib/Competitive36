b90b902faa3ee2d9826475b6bc2bf0fe84c1be2e
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-02 00:11:58 +0600

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n==1)
+        printf("I hate it");
+    else if(n>1)
+    {
+        for(i=1;i<n;i++)
+        {
+            if(i%2==1&&i!=n)
+                printf("I hate that ");
+            else if(i%2==0&&i!=n)
+                printf("I love that ");
+        }
+    }
+     if(n%2==0 && n>1)
+        printf(" I love it ");
+    else if(n>1)
+        printf(" I hate it ");
+ 
+ 
+ 
+ 
+    return 0;
+}
