d3eea5cb69935c4d2dacb960e6a1c8a18d2523cd
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-03 13:05:51 +0600

    Add Magnets [A] from Codeforces

---
+++ 344/A - Magnets/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main() {
+    int n,c=0;
+    scanf("%d",&n);
+    int a[100000];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+      
+    }
+    for(int i=0;i<n;i++)
+    {
+          if(a[i]!=a[i+1])
+        c++;
+    
+    }
+    printf("%d",c);
+ 
+    return 0;
+}
