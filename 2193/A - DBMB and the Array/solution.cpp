63244cecbfe79055edda6511b7ec1010831cc3c9
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:27:14 +0600

    Add DBMB and the Array [A] from Codeforces

---
+++ 2193/A - DBMB and the Array/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--){
+      int n,s,x;
+      cin>>n>>s>>x;
+      int arr[n];
+      for(int i=0;i<n;i++){
+          cin>>arr[i];
+      }
+      int sum=0;
+      for(int val:arr){
+          sum+=val;
+      }
+      if(sum>s){
+          cout<<"NO"<<endl;
+      }
+      else{
+           if((s-sum)%x==0){
+          cout<<"YES"<<endl;
+      }
+      else{
+          cout<<"NO"<<endl;
+      }
+      }
+     
+  }
+ 
+    return 0;
+}
