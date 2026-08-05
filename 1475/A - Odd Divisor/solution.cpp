0bdf4455129ade35041788d24e8b21cdf4728860
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:42:24 +0600

    Add Odd Divisor [A] from Codeforces

---
+++ 1475/A - Odd Divisor/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+ 
+        if (n % 2 == 1 || (n & (n - 1)) != 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+ 
+    return 0;
+}
