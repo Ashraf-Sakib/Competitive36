b2560a243dbb1612512581c5bdb46be7ad526840
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:48:34 +0600

    Add Park Lighting [A] from Codeforces

---
+++ 1358/A - Park Lighting/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+     int t;
+     cin>>t;
+     while(t--)
+     {
+        int n,m;
+        cin>>n>>m;
+        int ans=(n*m+1)/2;
+        cout<<ans<<endl;
+     }
+    return 0;
+}
