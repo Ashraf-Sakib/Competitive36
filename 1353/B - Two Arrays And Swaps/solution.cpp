e581a91630115cf3ef6f05b0275d3e518fe94ea4
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:52:55 +0600

    Add Two Arrays And Swaps [B] from Codeforces

---
+++ 1353/B - Two Arrays And Swaps/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    // Write your code here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end(),greater<int>());
+        for(int i=0;i<k;i++){
+            if(a[i]<b[i]){
+                a[i]=b[i];
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
