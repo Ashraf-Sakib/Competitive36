489dca67133d55caaeb3c1fb3d22025e15c8c5c0
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:44:02 +0600

    Add I'm bored with life [A] from Codeforces

---
+++ 822/A - I'm bored with life/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+     int a,b;
+     cin>>a>>b;
+     int ans=1;
+     for(int i=1;i<=min(a,b);i++)
+     {
+        ans*=i;
+     }
+     cout<<ans<<endl;
+    return 0;
+}
