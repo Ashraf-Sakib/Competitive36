d5577775c538f97dd847cfe5cb8c39c53bf0470b
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:50:41 +0600

    Add Two Elevators [A] from Codeforces

---
+++ 1729/A - Two Elevators/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int t1, t2;
+        t1 = abs(a - 1);
+        t2 = abs(b - c) + abs(c - 1);
+        if (t1 > t2)
+        {
+            cout << 2 << endl;
+        }
+        else if (t1 < t2)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 3 << endl;
+        }
+    }
+    return 0;
+}
