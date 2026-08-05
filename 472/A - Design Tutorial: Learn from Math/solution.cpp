0167748069da88d9d34f5d9e41471ce522b6a18c
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:42:15 +0600

    Add Design Tutorial: Learn from Math [A] from Codeforces

---
+++ 472/A - Design Tutorial: Learn from Math/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+     int n;
+     cin>>n;
+     int x,y;
+     if(n%2==0)
+     {
+        cout<<4<<" "<<n-4<<endl;
+     }
+     else
+     {
+        cout<<9<<" "<<n-9<<endl;
+     }
+    return 0;
+}
