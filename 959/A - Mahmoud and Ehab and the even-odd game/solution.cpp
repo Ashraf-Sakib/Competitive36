021babf38b7e742444ca52aff4b75f1d84257eb8
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:54:13 +0600

    Add Mahmoud and Ehab and the even-odd game [A] from Codeforces

---
+++ 959/A - Mahmoud and Ehab and the even-odd game/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    // Write your code here
+     int n;
+     cin>>n;
+     if( n%2==0)
+     {
+        cout<<"Mahmoud"<<endl;
+     }
+     else{
+        cout<<"Ehab"<<endl;
+     }
+    return 0;
+}
