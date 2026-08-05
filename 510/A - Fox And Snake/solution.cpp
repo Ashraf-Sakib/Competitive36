312f0b746073c937a8107abfe505de51f032a202
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:39:27 +0600

    Add Fox And Snake [A] from Codeforces

---
+++ 510/A - Fox And Snake/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+     int n,m;
+     cin>>n>>m;
+     int a=0;
+     for(int i=0;i<n;i++)
+     {
+        if(i%2==0)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<"#";
+            }
+        }
+        else
+        {
+            a++;
+            for(int j=0;j<m;j++)
+            {
+                if(a%2==1&&j==m-1)
+                {
+                    cout<<"#";
+                }
+                else if(a%2==0&&j==0)
+                {
+                    cout<<"#";
+                }
+                else
+                {
+                    cout<<".";
+                }
+            }
+        }
+        cout<<endl;
+     }
+    return 0;
+}
