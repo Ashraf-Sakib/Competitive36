37e6e52d3a875da28bf2b462cb86fbb2cf401d4f
Md. Ashraful Hasan <129622348+Ashraf-Sakib@users.noreply.github.com>
2026-08-05 22:45:47 +0600

    Add Shuffle Party [A] from Codeforces

---
+++ 1937/A - Shuffle Party/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Write your code here
+     int t;
+     cin>>t;
+     while(t--)
+     {
+        int n;
+        cin>>n;
+        int ans=1;
+        while(ans*2<=n)
+        {
+            ans*=2;
+        }
+        cout<<ans<<endl;
+     }
+    return 0;
+}
